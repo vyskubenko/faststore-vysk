7a325ca49d6817f82673d095b1706b8bd85eaeff
Lucas Vyskubenko <lucas.vysk@vtex.com.br>
2024-06-06 20:21:43 -0300

    shelf with video finished

--- src/Components/ShelfWithVideo/ShelfWithVideo.tsx
+++ src/Components/ShelfWithVideo/ShelfWithVideo.tsx
@@ -79,7 +79,7 @@ const ShelfWithVideo: FC<ShelfWithVideoProps> = ({
         data-fs-hero-image
       >
         <picture data-fs-video>
-          <iframe width="100%" height="100%" src={video} title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
+          <iframe width="100%" height="100%" src={video} title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </picture>
       </div>
     </section>

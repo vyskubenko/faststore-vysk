73a50348d756e3ee6112a4d5ecee047f67618770
Lucas Vyskubenko <lucas.vysk@vtex.com.br>
2024-06-19 17:31:45 -0300

    Delete src/Components directory

--- src/Components/Search/index.tsx
+++
@@ -1,5 +0,0 @@
-import SearchContainer from "./SearchContainer"
-import SearchInput from "./SearchInput"
-import SearchOpenButton from "./SearchOpenButton"
-
-export { SearchInput, SearchOpenButton, SearchContainer }
